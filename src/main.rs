15b5b76a14cdd2b370893eb2197e3d081905f143
tiago-lp <tiagolprr@gmail.com>
2020-12-02 22:35:09 -0300

    barrier

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod rendezvous;
+mod multiplex;
+mod barrier;
+mod reusable_barrier;
 
 fn main() {
     rendezvous::rendezvous();
+    multiplex::multiplex();
+    barrier::barrier();
+    reusable_barrier::reusable_barrier();
     println!("Hello, world!");
 }
