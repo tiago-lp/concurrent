96b5c0b926f7f3b7e45561b737d889af775e40bb
wesley.anibal <wesley.anibal@ccc.ufcg.edu.br>
2020-12-02 22:50:51 -0300

    mutex implementado

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod rendezvous;
 mod multiplex;
 mod barrier;
 mod reusable_barrier;
+mod mutex;
 
 fn main() {
     rendezvous::rendezvous();
     multiplex::multiplex();
     barrier::barrier();
     reusable_barrier::reusable_barrier();
+    mutex::mutex();
     println!("Hello, world!");
 }
--- src/mutex.rs
+++ src/mutex.rs
@@ -1,1 +1,8 @@
 use std_semaphore::Semaphore;
+use std::sync::Mutex;
+
+pub fn mutex() {
+    use std::sync::Mutex;
+
+    let mutex = Mutex::new(0);
+}
